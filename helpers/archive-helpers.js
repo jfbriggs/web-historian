1c713cbde0aa8f717559a7cb47bdf1ce5edd6c2e
Hack Reactor Students <accounts@hackreactor.com>
2017-01-11 20:25:29 -0800

    Pass most tests, requests TBD

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var request = require('request');
+var requestHandler = require('../web/request-handler');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -25,17 +27,56 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
-exports.readListOfUrls = function() {
+exports.readListOfUrls = function(callback) {
+  var urls;
+  fs.readFile(exports.paths.list, 'utf8', function(err, data) {
+    urls = data.toString().split('\n');
+    callback(err, urls);
+  });
+
 };
 
-exports.isUrlInList = function() {
+exports.isUrlInList = function(url, callback) {
+  exports.readListOfUrls(function(err, urls) {
+    if (!err) {
+      var exists = false;
+      urls.forEach(function(item) {
+        if (item === url) {
+          exists = true;
+        }
+      });
+      callback(err, exists);
+    }
+  });
 };
 
-exports.addUrlToList = function() {
+exports.addUrlToList = function(url, callback) {
+  fs.appendFile(exports.paths.list, url + '\n', function(err) {
+    if (err) {
+      throw err;
+    } else {
+      callback();
+      console.log('Added ' + url + ' to list!');
+    }
+  });
 };
 
-exports.isUrlArchived = function() {
+exports.isUrlArchived = function(url, callback) {
+  fs.stat(path.join(exports.paths.archivedSites, url), function(err, stats) {
+    if (err) { 
+      callback(null, false);
+    } else {
+      callback(null, true);
+    }
+  });
 };
 
-exports.downloadUrls = function() {
+exports.downloadUrls = function(urls) {
+  urls.forEach(function(url) {
+    request('http://' + url).pipe(
+      fs.createWriteStream(path.join(exports.paths.archivedSites, url))
+    );
+    console.log(url, 'saved to archive.');
+  });
+
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -6,11 +6,46 @@ var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
   res.writeHeader(200, helpers.headers);
+  console.log(req.url);
+  console.log(req.method);
+  var slicedUrl;
 
-  // if index.html is being requested
+  
+  if (req.method === 'POST') {
+    var body = [];
+    var slicedUrl;
+
+    req.on('data', function(chunk) {
+      body.push(chunk);
+    }).on('end', function() {
+      body = Buffer.concat(body).toString();
+      slicedUrl = body.slice(4);
+      // Run function to add the url to sites.txt
+      archive.addUrlToList(slicedUrl, function() {
+        console.log('Added', slicedUrl, 'to be downloaded.');
+        //SERVE UP LOADING PAGE
+      });
+
+      res.writeHeader(302, helpers.headers);
+      res.end();
+    });
+
+    // archive.isUrlInList(body, function(err, exists) {
+    //   if (!err && !exists) {
+    // archive.addUrlToList(slicedUrl, function() {
+    //   console.log('Added', slicedUrl, 'to be downloaded.');
+    //   //SERVE UP LOADING PAGE
+    // });
+    //   } else if (!err && exists) {
+    //     SERVE UP LOADING PAGE
+    //   }
+    // });
+  }
+  // INITIAL LOAD: INDEX.HTML IS BEING REQUESTED
   helpers.serveAssets(res, path.join(archive.paths.siteAssets, 'index.html'), function(data) {
     res.end(data);
   });
+
 
   // fs.readFile(path.join(archive.paths.siteAssets, 'index.html'), 'utf8', function(err, data) {
   //   if (err) {
